6e23f4cc3b52bf8cfcb8ec1ee78b6aa2fc15e8c5
Bodhitha Weerasekara <88415912+IT20204334@users.noreply.github.com>
2022-09-09 08:06:15 +0530

    backform

--- studio-management-be/src/app.module.ts
+++ studio-management-be/src/app.module.ts
@@ -3,13 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventOrderModule } from './event order/eventOrder.module';
-<<<<<<< HEAD
 import { ProfessionalOrderModule } from './professional order/professionalOrder.module';
-
-const DB_URL = `mongodb+srv://Kelum:SPM1234@spm.tnya5xw.mongodb.net/?retryWrites=true&w=majority`;
-@Module({
-  imports: [MongooseModule.forRoot(DB_URL), EventOrderModule, ProfessionalOrderModule],
-=======
 import { AmateurOrderModule } from './amateur order/amaterOrder.module';
 
 const DB_URL = `mongodb+srv://Kelum:SPM1234@spm.tnya5xw.mongodb.net/?retryWrites=true&w=majority`;
@@ -18,8 +12,8 @@ const DB_URL = `mongodb+srv://Kelum:SPM1234@spm.tnya5xw.mongodb.net/?retryWrites
     MongooseModule.forRoot(DB_URL),
     EventOrderModule,
     AmateurOrderModule,
+    ProfessionalOrderModule
   ],
->>>>>>> 17b20c0983a06b6fecb10b3d3f212c0cf293e008
   controllers: [AppController],
   providers: [AppService],
 })
